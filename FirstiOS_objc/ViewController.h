68ee20a70c24483893fa0caa31389e56924b1c9c
Yitian <chenyitian_best@163.com>
2020-11-18 16:38:23 +0800

    Initial Commit

---
+++ FirstiOS_objc/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FirstiOS_objc
+//
+//  Created by Yitian Chen on 2020/11/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstiOS_objc/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  FirstiOS_objc
+//
+//  Created by Yitian Chen on 2020/11/18.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
